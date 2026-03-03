8912246fd2d51161b8b9f4dddee28bfb89d469cf
Vinvinn <gorgoniobradlyvince@gmail.com>
2026-03-03 22:21:01 +0800

    Import useEffect from react in index.jsx
    Add missing useEffect import to pages/index.jsx so React hooks can be used in the component and to avoid potential undefined reference errors when adding side-effect logic.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import { useRouter } from 'next/router'
+import { useEffect } from 'react';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
