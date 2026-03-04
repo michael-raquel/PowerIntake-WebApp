eebe9a4e057b3a2eeb7abbd9f68fc9cb7093d130
DanielGals <janjangallaron@gmail.com>
2026-03-04 18:55:15 +0800

    Revert Test remove if error

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
   const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
     async function fetchUser() {
@@ -10,10 +11,17 @@ export default function App({ Component, pageProps }) {
       const data = await res.json();
       if (data.clientPrincipal) {
         setUser(data.clientPrincipal);
+
+        // The token is inside the claims or via the X-MS-TOKEN headers
+        // For AAD, you can get it this way:
+        const idToken = data.clientPrincipal?.claims?.find(
+          (c) => c.typ === "idp_access_token"
+        )?.val;
+        setToken(idToken);
       }
     }
     fetchUser();
   }, []);
 
-  return <Component {...pageProps} user={user} />;
+  return <Component {...pageProps} user={user} token={token} />;
 }
--- pages/user/home/index.jsx
+++ pages/user/home/index.jsx
@@ -1,8 +1,21 @@
-// pages/user/home.jsx
-export default function UserHome() {
+export default function UserHome({ user, token }) {
+  console.log(token);
+  async function callExternalApi() {
+    const res = await fetch("https://your-api.com/endpoint", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+  }
+
   return (
     <div>
-      <h1>Welcome to Home </h1>
+      <h1>Welcome, {user?.userDetails}</h1>
+      <h1>Token: {token}</h1>
+      <button onClick={callExternalApi}>Call API</button>
     </div>
-  )
+  );
 }
