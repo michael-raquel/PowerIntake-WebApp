c52cdda7bafc61565b4da1ceb71af515e08e980b
DanielGals <janjangallaron@gmail.com>
2026-03-05 22:23:26 +0800

    Revert "Add PWA manifest, icons, service worker, and sharp"
    This reverts commit 110f75e34dc49ab365328d0284c9cb0425e60e2c.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,4 @@
 import "@/styles/globals.css";
-import { useEffect } from "react";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "@/lib/msalConfig";
@@ -7,21 +6,6 @@ import { msalConfig } from "@/lib/msalConfig";
 const msalInstance = new PublicClientApplication(msalConfig);
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", () => {
-        navigator.serviceWorker
-          .register("/sw.js")
-          .then((reg) =>
-            console.log("[PWA] Service Worker registered:", reg.scope),
-          )
-          .catch((err) =>
-            console.error("[PWA] Service Worker registration failed:", err),
-          );
-      });
-    }
-  }, []);
-
   return (
     <MsalProvider instance={msalInstance}>
       <Component {...pageProps} />
